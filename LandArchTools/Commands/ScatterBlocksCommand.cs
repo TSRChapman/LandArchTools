6f7446f9e51d248f326ec219b98741d783c71a63
Thomas Chapman <63542034+TSRChapman@users.noreply.github.com>
2024-02-18 16:19:49 +1100

    working through scatter blocks

--- LandArchTools/Commands/ScatterBlocksCommand.cs
+++ LandArchTools/Commands/ScatterBlocksCommand.cs
@@ -31,9 +31,38 @@ namespace LandArchTools.Commands
 
                 // Get user options
                 Result rs = getUserOptions(out ObjRef surface, out ObjRef[] blocks, out int numBlocks, out double scale, out bool rotation);
+                if (rs != Result.Success)
+                {
+                    RhinoApp.WriteLine("Failed to get user options");
+                    return Result.Failure;
+                }
+                
+                // Turn redraw off for performance
+                doc.Views.RedrawEnabled = false;
+
+                // Test if surface is a mesh If not a mesh, convert to mesh
+                Mesh mesh = new Mesh();
+                var isMesh = surface.Object().Geometry.ObjectType == ObjectType.Mesh;
+                if (isMesh == false)
+                {
+                    mesh = convertToMesh(surface);
+                    if (mesh == null)
+                    {
+                        RhinoApp.WriteLine("Failed to convert surface to mesh");
+                        return Result.Failure;
+                    }
+                }
+                else
+                {
+                    mesh = surface.Object().Geometry as Mesh;
+                }
 
 
 
+
+                // Turn Redraw back on
+                doc.Views.RedrawEnabled = true;
+                // Close script
                 doc.Views.Redraw();
                 return Result.Success;
             }
@@ -42,9 +71,49 @@ namespace LandArchTools.Commands
                 RhinoApp.WriteLine("Failed to execute");
                 doc.Views.RedrawEnabled = true;
                 return Result.Failure;
-            }   
+            }
         }
 
+        /// <summary>
+        /// Converts a surface or brep into a mesh
+        /// </summary>
+        /// <param name="surface"></param>
+        /// <returns>
+        /// returns a mesh if successful, null if failed
+        /// </returns>
+        private static Mesh convertToMesh(ObjRef surface)
+        {
+            // convert geometry to brep
+            RhinoObject surfaceObj = surface.Object();
+            if (surfaceObj.Geometry.HasBrepForm == false) return null;
+            Brep surfaceBrep = Brep.TryConvertBrep(surfaceObj.Geometry);
+
+            // Convert to mesh and rebuild mesh parts
+            Mesh mesh = new Mesh();
+            Mesh[] meshParts = Mesh.CreateFromBrep(surfaceBrep, MeshingParameters.QualityRenderMesh);
+            foreach (var i in meshParts)
+            {
+                mesh.Append(i);
+            }
+
+            if (mesh == null)
+            {
+                RhinoApp.WriteLine("Failed to convert surface to mesh");
+                return null;
+            }
+
+            return mesh;
+        }
+
+        /// <summary>
+        /// Gets user options for the scatter command
+        /// </summary>
+        /// <param name="surface"></param>
+        /// <param name="blocks"></param>
+        /// <param name="numBlocks"></param>
+        /// <param name="scale"></param>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
         private static Result getUserOptions(out ObjRef surface, out ObjRef[] blocks, out int numBlocks, out double scale, out bool rotation)
         {
             surface = null;
@@ -54,52 +123,35 @@ namespace LandArchTools.Commands
             rotation = false;
 
             // Select surface to scatter on
-            var gSurface = new GetObject();
-            gSurface.SetCommandPrompt("Select surface to scatter on");
-            gSurface.GeometryFilter = ObjectType.Surface | ObjectType.Brep | ObjectType.Mesh;
-            gSurface.SubObjectSelect = false;
-            gSurface.Get();
-            if (gSurface.CommandResult() != Result.Success) return gSurface.CommandResult(); 
-            surface = gSurface.Object(0);
+            ObjRef surfaceChoice;
+            Result r1 = RhinoGet.GetOneObject("Select surface to scatter on", false, ObjectType.Surface | ObjectType.Brep | ObjectType.Mesh, out surfaceChoice);
+            if (r1 != Result.Success) return r1;
+            surface = surfaceChoice;
 
             // Select blocks to scatter
-            var gBlocks = new GetObject();
-            gBlocks.SetCommandPrompt("Select blocks to scatter");
-            gBlocks.GeometryFilter = ObjectType.InstanceReference;
-            gBlocks.GroupSelect = true;
-            gBlocks.GetMultiple(1, 0);
-            if (gBlocks.CommandResult() != Result.Success) return gBlocks.CommandResult();
-            blocks = gBlocks.Objects();
+            ObjRef[] blockRefs;
+            Result r2 = RhinoGet.GetMultipleObjects("Select blocks to scatter", false, ObjectType.InstanceReference, out blockRefs);
+            if (r2 != Result.Success) return r2;
+            blocks = blockRefs;
 
             // get int of blocks to scatter
-            var gInt = new GetInteger();
-            gInt.SetCommandPrompt("Number of blocks to scatter");
-            gInt.AcceptNothing(false);
-            gInt.SetDefaultNumber(1);
-            gInt.SetLowerLimit(1, false);
-            gInt.SetUpperLimit(10000, false);
-            gInt.Get();
-            if (gInt.CommandResult() != Result.Success) return gInt.CommandResult();
-            numBlocks = gInt.Number();
+            int numChoice = 1;
+            Result r3 = RhinoGet.GetInteger("Enter number of blocks to scatter", true, ref numChoice, 1, 10000);
+            if (r3 != Result.Success) return r3;
+            numBlocks = numChoice;
 
             // get double for scaling of objects
-            var gNum = new GetNumber();
-            gNum.SetCommandPrompt("enter scale multiplyer (0 for no scaling)");
-            gNum.SetDefaultNumber(0);
-            gNum.Get();
-            if (gNum.CommandResult() != Result.Success) return gNum.CommandResult();
-            scale = gNum.Number();
+            double scaleChoice = 0;
+            Result r4 = RhinoGet.GetNumber("Enter scale multiplyer (0 for no scaling)", true, ref scaleChoice, 0, 1000);
+            if (r4 != Result.Success) return r4;
+            scale = scaleChoice;
 
-            // get bool for rotation of objects
-            var gBool = new GetOption();
-            gBool.SetCommandPrompt("random rotation of blocks?");
-            var opt1 = gBool.AddOption("Yes");
-            var opt2 = gBool.AddOption("No");
-            gBool.Get();
-            if (gBool.CommandResult() != Result.Success) return gBool.CommandResult();
-            if (gBool.Option().Index == opt1) rotation = true;
-            else rotation = false;
-
+            // get user input for yes or no on rotation
+            bool rotationChoice = false;
+            Result r5 = RhinoGet.GetBool("Rotate Objects?", true, "No", "Yes", ref rotationChoice);
+            if (r5 != Result.Success) return r5;
+            rotation = rotationChoice;
+            
             return Result.Success;
 
         }
