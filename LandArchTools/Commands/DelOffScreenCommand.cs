c28edf7922d0588efd822c9b8b5b359c1cff9f9a
Thomas Chapman <63542034+TSRChapman@users.noreply.github.com>
2024-03-02 22:37:49 +1100

    added facecamera and deloffscreen commands

---
+++ LandArchTools/Commands/DelOffScreenCommand.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace LandArchTools.Commands
+{
+    public class DelOffScreenCommand : Command
+    {
+        public DelOffScreenCommand()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static DelOffScreenCommand Instance { get; private set; }
+
+        public override string EnglishName => "DelOffScreen";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            try
+            {
+                // Select objects to test
+                var getObject = new GetObject();
+                getObject.SetCommandPrompt("Select objects to test");
+                getObject.GroupSelect = true;
+                getObject.SubObjectSelect = false;
+                getObject.GetMultiple(1, 0);
+                if (getObject.CommandResult() != Result.Success)
+                    return getObject.CommandResult();
+
+                var objects = getObject.Objects();
+
+                // Ask user if they want to delete or hide objects
+                var options = new GetOption();
+                options.SetCommandPrompt("Delete or Hide");
+                int deleteOptionIndex = options.AddOption("Delete");
+                int hideOptionIndex = options.AddOption("Hide");
+                options.Get();
+                if (options.CommandResult() != Result.Success)
+                    return options.CommandResult();
+
+                bool delete = options.OptionIndex() == deleteOptionIndex;
+
+                // Disable screen redraw
+                doc.Views.RedrawEnabled = false;
+
+                foreach (var objRef in objects)
+                {
+                    var obj = objRef.Object().Geometry;
+                    if (obj == null) continue;
+
+                    // current active view
+                    var activeView = doc.Views.ActiveView;
+
+                    bool isVisible = activeView.MainViewport.IsVisible(obj.GetBoundingBox(false));
+
+                    // If the object is not visible, delete or hide it based on user choice
+                    if (!isVisible)
+                    {
+                        if (delete)
+                        {
+                            doc.Objects.Delete(objRef, true);
+                        }
+                        else
+                        {
+                            doc.Objects.Hide(objRef, true);
+                        }
+                    }
+                }
+
+                // Enable screen redraw
+                doc.Views.RedrawEnabled = true;
+                doc.Views.Redraw();
+
+                return Result.Success;
+            }
+            catch (Exception ex)
+            {
+                RhinoApp.WriteLine($"Failed to execute: {ex.Message}");
+                doc.Views.RedrawEnabled = true;
+                return Result.Failure;
+            }
+        }
+
+    }
+}
---
+++ LandArchTools/Commands/FaceCameraCommand.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+using Rhino.Input;
+
+namespace LandArchTools.Commands
+{
+    public class FaceCameraCommand : Command
+    {
+        public FaceCameraCommand()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static FaceCameraCommand Instance { get; private set; }
+
+        public override string EnglishName => "FaceCamera";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            try
+            {
+                Result r1 = getUserOptions(out ObjRef[] surfaces);
+                if (r1 != Result.Success)
+                    return r1;
+
+                rotateSurfaces(surfaces, doc);
+
+                doc.Views.Redraw();
+                return Result.Success;
+            }
+            catch (Exception ex)
+            {
+                RhinoApp.WriteLine($"Failed to execute: {ex.Message}");
+                return Result.Failure;
+            }
+        }
+
+        private static Result rotateSurfaces(ObjRef[] surfaces, RhinoDoc doc)
+        {
+            // get camera location
+            Point3d cameraPt = doc.Views.ActiveView.ActiveViewport.CameraLocation;
+            cameraPt.Z = 0;
+
+            foreach (ObjRef surface in surfaces)
+            {
+                // get surface object
+                Surface srf = surface.Surface();
+                if (srf == null)
+                    return Result.Failure;
+
+                // reparameterize surface
+                srf.SetDomain(0, new Interval(0, 1));
+                srf.SetDomain(1, new Interval(0, 1));
+
+                // get midpoint of surface and set Z coordinate to 0
+                var midPt = srf.PointAt(0.5, 0.5);
+                midPt.Z = 0;
+
+                // get normal of surface
+                Vector3d normal = srf.NormalAt(0.5, 0.5);
+                normal.Z = 0;
+
+                // get vector from camera to midpoint
+                Vector3d cameraToSurface = midPt - cameraPt;
+
+                // calculate rotation axis using cross product
+                Vector3d rotationAxis = Vector3d.CrossProduct(normal, cameraToSurface);
+
+                // get angle between normal and camera to surface
+                double angle = Vector3d.VectorAngle(normal, cameraToSurface);
+
+                // rotate the surface to face the camera
+                Transform rotation = Transform.Rotation(angle, rotationAxis, midPt);
+                doc.Objects.Transform(surface, rotation, true);
+            }
+
+            return Result.Success;
+        }
+
+        /// <summary>
+        /// Gets user options for the scatter command
+        /// </summary>
+        /// <param name="surface"></param>
+        /// <param name="blocks"></param>
+        /// <param name="numBlocks"></param>
+        /// <param name="scale"></param>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
+        private static Result getUserOptions(out ObjRef[] surface)
+        {
+            surface = null;
+
+            // Select surface to scatter on
+            ObjRef[] surfaceChoice;
+            Result r1 = RhinoGet.GetMultipleObjects(
+                "Select surfaces to face towards camera",
+                false,
+                ObjectType.Surface,
+                out surfaceChoice
+            );
+
+            if (r1 != Result.Success)
+                return r1;
+
+            surface = surfaceChoice;
+            return Result.Success;
+        }
+    }
+}
